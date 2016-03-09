35cf06cb8d54671aa093179164ed5ad1b7d39736
KPostOffice <k.postlethwait24@gmail.com>
2016-03-09 14:14:16 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCUtype html>
+<html>
+  <header>Login</header>
+  <body>
+    <div id=container>
+      <div id=loginWindow>
+        <h1>Class Engage</h1>
+        <form>
+          Username:<br>
+          <input value="Username" type="text" name="user"><br>
+          Password:<br>
+          <input value="Password" type="text" name="pass">
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
