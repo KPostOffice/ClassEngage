3e402fa2df9ec47de37b459276911af33828da90
KPostOffice <k.postlethwait24@gmail.com>
2016-03-11 14:07:55 -0500

    Update userPage.php

--- userPage.php
+++ userPage.php
@@ -1,6 +1,28 @@
 <?php
-  if($_PUTS("username")=="teacher")
+  protected siteType=0;
+  if($_POST("user")=="teacher")
   {
-    
+    if($_POST("pass")=="password")
+    {
+      siteType=1;
+    }
+  }
+  else if($_POST("user")=="Kevin")
+  {
+    if($_POST("pass")==password)
+    {
+      siteType=2;
+    }
+  }
+  if(siteType==0)
+  {
+    header("Location = 'login.php?ERROR=1'");
   }
 ?>
+<!DOCUtype html>
+<html>
+  <header>UserPage</header>
+  <body>
+    <h1><?php if(siteType==1){echo "I'm a teacher";}else{echo "I am a student";} ?></h1>
+  </body>
+</html>
