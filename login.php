b8753898718185d70f50c5da8ae90b4c4b6f5184
KPostOffice <k.postlethwait24@gmail.com>
2016-03-11 13:50:44 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,13 +5,13 @@
     <div id=container>
       <div id=loginWindow>
         <h1>Class Engage</h1>
-        <form>
+        <form action="userPage.php" action="post">
           Username:<br>
           <input type="text" name="user"><br>
           Password:<br>
           <input type="password" name="pass">
           <br>
-          <input value="Login" type="button" name="go">
+          <input value="Login" type="submit" name="login">
         </form>
       </div>
     </div>
