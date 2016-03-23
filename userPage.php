d3762224a9f5a6aebeb2fe54d51dfc1e2832bfc3
KPostOffice <k.postlethwait24@gmail.com>
2016-03-23 13:58:04 -0400

    Update userPage.php

--- userPage.php
+++ userPage.php
@@ -1,33 +1,33 @@
 <?php
-  protected siteType=0;
-  if(!$_POST)
+  $siteType=0;
+  if(empty($_POST))
   {
     header("Location = 'login.php?ERROR=1'")
   }
-  if($_POST("user")=="teacher")
+  if($_POST["user"]=="teacher")
   {
-    if($_POST("pass")=="password")
+    if($_POST["pass"]=="password")
     {
-      siteType=1;
+      $siteType=1;
     }
   }
-  else if($_POST("user")=="Kevin")
+  else if($_POST["user"]=="Kevin")
   {
-    if($_POST("pass")=="password")
+    if($_POST["pass"]=="password")
     {
-      siteType=2;
+      $siteType=2;
     }
   }
-  if(siteType==0)
+  if($siteType==0)
   {
     header("Location = 'login.php?ERROR=1'");
   }
 ?>
-<!DOCUtype html>
+<!DOCtype html>
 <html>
   <header>UserPage</header>
   <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
   <body>
-    <h1><?php if(siteType==1){echo "I'm a teacher";}else{echo "I am a student";} ?></h1>
+    <h1><?php if($siteType==1){echo "I'm a teacher";}else{echo "I am a student";} ?></h1>
   </body>
 </html>
