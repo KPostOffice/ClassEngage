d7405bf790fcc04e9046be405da22e1eba8e5cfe
KPostOffice <k.postlethwait24@gmail.com>
2016-04-05 13:46:35 -0400

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
       <div id=loginWindow>
         <h1>Class Engage</h1>
         <p style="color:red"><?php if($_GET["ERROR"]=="1"){echo "Username/password incorrect";}?>
-        <form action="userPage.php" action="post">
+        <form action="userPage.php" method="post">
           Username:<br>
           <input type="text" name="user"><br>
           Password:<br>
